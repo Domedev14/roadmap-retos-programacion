65dc49d03cd6d12019d1e455a8af5d6680184e2a
Domedev14 <168467120+Domedev14@users.noreply.github.com>
2024-05-04 11:24:52 +0200

    Create DomeneDev14.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DomeneDev14.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+# Esto es un comentario de una linea
+
+"""
+Esto es
+un comentario
+varias lineas
+"""
+
+# Crear una variable
+my_first_var = "Primera variable"
+
+# Tipos de datos primitivos
+
+# Enteros
+my_int = 5
+# Flotantes
+my_float = 5.5
+# Boolean
+my_boolean = True
+# String
+my_string = "Cadena de texto"
+# Listas
+my_list = [1, 2, 3, 4, 5]
+# Tuplas inmutables
+my_tupla = (1, 2, 3, 4, 5)
+# Set elementos unicos y desordenados, no soporta indices
+my_set = {1, 2, 3, 4, 5}
+# Diccionarios
+my_diccionary = {"Primero": 1, "Segundo": 2, "Tercero": 3}
+
+hello = "Hola, Python"
+print(hello)
